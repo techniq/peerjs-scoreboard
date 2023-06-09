5b48b8b1222c81c67e17af2cd9870a4ac699868f
Sean Lynch <techniq35@gmail.com>
2023-06-09 19:59:59 -0400

    Add instructions and swap order tabs (logical usage)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,8 +20,8 @@
 <header>
 	<ToggleGroup value={$page.url.pathname} underlined on:change={(e) => goto(e.detail.value)}>
 		<ToggleOption value="/" class="w-32">Home</ToggleOption>
-		<ToggleOption value="/send" class="w-32">Send</ToggleOption>
 		<ToggleOption value="/receive" class="w-32">Receive</ToggleOption>
+		<ToggleOption value="/send" class="w-32">Send</ToggleOption>
 	</ToggleGroup>
 </header>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,27 @@
 <script lang="ts">
 </script>
 
-Choose to Send or Receive
+<h1 class="text-xl">Instructions</h1>
+
+<section class="mt-6">
+	<h2 class="text-lg">Receive</h2>
+	<ul class="list-inside list-disc">
+		<li>Display scores</li>
+		<li>Clicking on the top or bottom of a score will increment or decrement, respectively</li>
+		<li>
+			Update the scores remotely by referencing the 4 character code on another device (or more)
+			using the `Send` page
+		</li>
+		<li>Recommend using a larger device such as tablet or computer</li>
+	</ul>
+</section>
+
+<section class="mt-6">
+	<h2 class="text-lg">Send</h2>
+	<ul class="list-inside list-disc">
+		<li>
+			Update scores displayed on another device's Receive page using the 4 character receive code
+		</li>
+		<li>Recommend using a smaller device such as phone</li>
+	</ul>
+</section>
